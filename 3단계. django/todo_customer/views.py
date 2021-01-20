f8d15cae347059b8ea3f59ffeb9cee3a3e529453
SangRakKun <leesangrak0307@gmail.com>
2021-01-20 15:09:54 +0900

    로그인 기능 추가

--- 3단계. django/todo_customer/views.py
+++ 3단계. django/todo_customer/views.py
@@ -1,33 +1,35 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+#from django.http import HttpResponse,HttpResponseRedirect
 
 def signup(request):
     if request.method =="POST":
         if request.POST["password1"] == request.POST["password2"]:
             user =User.objects.create_user(username=request.POST["username"],password=request.POST["password1"])
             auth.login(request,user)
-            return redirect('home')
-        return render(request,'signup.html')
+            return redirect('index')
+        return render(request,'todo_customer/signup.html')
 
-    return render(request,'signup.html')
+    return render(request,'todo_customer/signup.html')
 
 def login(request):
     if request.method=="POST":
         username=request.POST['username']
-        password =request.POST['password']
+        password=request.POST['password']
         user = auth.authenticate(request,username=username,password=password)
         if user is not None:
             auth.login(request,user)
-            return redirect('home')
+            return redirect('index')
         else:
-            return render(request, 'login.html', {'error' : 'username or password is incorrect'})
+            return render(request, 'todo_customer/login.html', {'error' : 'username or password is incorrect'})
     else:
-        return render(request,'login.html')
+        return render(request,'todo_customer/login.html')
 
 
 def logout(request):
     auth.logout(request)
-    return redirect('home')
+    return redirect('index')
 
 # Create your views here.
